2d5d77b4614c44a6cdd6f47b1bdee2f0544d0b70
Andras <toth.andras.2000@gmail.com>
2022-10-07 15:13:16 +0200

    server response to react server

--- frontend/webshop-app/src/components/landingPage/landing-page-component.jsx
+++ frontend/webshop-app/src/components/landingPage/landing-page-component.jsx
@@ -1,8 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import { Navigation } from '../navigation/navigation-component.jsx'
 import { Footer } from '../footer/footer-components.jsx'
+import { getOrders } from '../../DATA/data-connect-to-server.js'
+import { useEffect } from 'react'
 
 export const LandingPage = () => {
+  useEffect(() => {
+    getOrders()
+    return () => {}
+  }, [])
+
   return (
     <section>
       <Navigation />
