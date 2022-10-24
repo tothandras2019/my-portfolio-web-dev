180471f867efac4420d2ef808d87a31df503bcf0
Andras <toth.andras.2000@gmail.com>
2022-10-24 15:34:30 +0200

    send files ready

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 9000
 const path = require('path')
 const cors = require('cors')
+const fs = require('fs')
 
 const whitelist = ['http://127.0.0.1:3000', 'http://localhost:3000']
 const options = {
@@ -12,8 +13,30 @@ const options = {
 app.use(cors(options))
 
 app.get('/', (req, res) => {
-  console.log('reg comes')
-  res.status(200).send({ success: true, message: 'this is home page message' })
+  console.log('provide all ship for homepage')
+  fs.readFile(path.join(__dirname, '/SHIPS_DATA/ships.json'), (err, data) => {
+    if (err) return res.status(400).send({ success: false, message: 'No availabe ships' })
+    const shipsData = JSON.parse(data)
+    res.status(200).send(shipsData)
+  })
+})
+
+app.get('/img/:id', (req, res) => {
+  const fileId = parseInt(req.params.id)
+
+  fs.readFile(path.join(__dirname, '/SHIPS_DATA/ships.json'), (err, data) => {
+    const shipsData = JSON.parse(data)
+    const ship = shipsData.find((shipObj) => (shipObj.id === fileId ? shipObj.img : null))
+    const options = {
+      root: path.join(__dirname, `/SHIPS_DATA/imgs/yachts-for-sale/`),
+    }
+
+    res.sendFile(`${ship.img}`, options, (err) => {
+      if (err) {
+        console.error('error', err)
+      } else console.log('file sent')
+    })
+  })
 })
 
 app.post('/order', (req, res) => {
